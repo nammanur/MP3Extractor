20b6f8e6c7214bd62ebc850011f308c4171b140d
narayanan <nammanur@gmail.com>
2022-05-24 11:05:25 +0530

    First Commit

---
+++ src/main/java/com/chennaitools/Mp3Extractor/Mp3ExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.chennaitools.Mp3Extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3ExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3ExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chennaitools/Mp3Extractor/Mp3ExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chennaitools.Mp3Extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp3ExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
